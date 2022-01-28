a937f094c3c8c05e0f6d6fa74efb464744a05936
sudheshnajinna <98102107+sudheshnareddy@users.noreply.github.com>
2022-01-29 04:03:59 +0530

    Delete Emailer.java

--- dependencyinversion/Emailer.java
+++
@@ -1,8 +0,0 @@
-package src.main.java.dependency_inversion_bad;
-
-public class Emailer {
-    public String generateWeatherAlert(String weatherConditions) {
-        String alert = "It is " + weatherConditions;
-        return alert;
-    }
-}
