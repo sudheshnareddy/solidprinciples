3a2b147c39f3cba6954d4587a3c9fbdda46c06ef
sudheshnajinna <98102107+sudheshnareddy@users.noreply.github.com>
2022-01-29 04:04:10 +0530

    Delete Phone.java

--- dependencyinversion/Phone.java
+++
@@ -1,8 +0,0 @@
-package src.main.java.dependency_inversion_bad;
-
-public class Phone {
-    public String generateWeatherAlert(String weatherConditions) {
-        String alert = "It is " + weatherConditions;
-        return alert;
-    }
-}
